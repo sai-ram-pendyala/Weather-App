45d7e084a62c1c3106563dc4aa576b3273c8b681
Sai Ram Pendyala <89296568+sai-ram-pendyala@users.noreply.github.com>
2023-06-18 11:55:10 +0530

    Add required CSS

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
+const input = document.querySelector('input');
+const btn = document.querySelector('button');
+const info = document.querySelector('#info');
+const loc = document.querySelector('#location');
+const dt = document.querySelector('#dateTime');
+const cond = document.querySelector('#condition');
+const icon = document.querySelector('#icon');
+const temp = document.querySelector('#temp');
+const feels = document.querySelector('#feelsLike');
+const hum = document.querySelector('#humidity');
+const clouds = document.querySelector('#clouds');
+const url = 'https://api.weatherapi.com/v1/current.json?key=7ff95b83f9ee4132b0044357231706';
+
+const img = document.createElement('img');
+
+
 async function getWeatherData() {
-    let response = await fetch('https://api.weatherapi.com/v1/current.json?key=7ff95b83f9ee4132b0044357231706&q=london&days=14', {mode: 'cors'});
-    let weatherData = await response.json();
-    console.log(weatherData);
+    try {
+        let response = await fetch(url + `&q=${input.value}`, {mode: 'cors'});
+        let weatherData = await response.json();
+        displayData(weatherData);
+    }
+    catch {
+        info.textContent = "Location not found. Please try again!!!";
+    }
 }
 
-getWeatherData();
+btn.addEventListener('click', () => {
+    getWeatherData();
+});
+
+displayData = (weatherData) => {
+    loc.textContent = weatherData.location.name + ', ' +weatherData.location.country;
+    dt.textContent = weatherData.current.last_updated;
+    cond.textContent = weatherData.current.condition.text;
+    img.src = weatherData.current.condition.icon;
+    icon.appendChild(img);
+    temp.textContent = "Temperature: " + weatherData.current.temp_c + ' °C';
+    feels.textContent = "Feels Like: " + weatherData.current.feelslike_c + ' °C';
+    hum.textContent = "Humidity: " + weatherData.current.humidity;
+    clouds.textContent = "Clouds: " + weatherData.current.cloud;
+}
